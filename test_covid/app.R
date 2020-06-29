b28cd947178dfbcda682fc5b1a9262746dcf3b92
mallen011 <60446506+mallen011@users.noreply.github.com>
2020-06-29 14:09:50 -0400

    Update app.R

--- test_covid/app.R
+++ test_covid/app.R
@@ -1,6 +1,12 @@
 ###LOAD LIBRARIES
 library(shiny)
 library(shinydashboard)
+library(leafpop)
+library(sf)
+library(tidyverse)
+library(extrafont)
+#font_import()
+#loadfonts(device = "win")
 
 ##sources for help
 #     https://stackoverflow.com/questions/21515800/subset-a-data-frame-based-on-user-input-shiny
@@ -10,13 +16,15 @@ library(shinydashboard)
 
 
 ## SET PATH ##
-path <- "C:/Users/user/Desktop/Countyapp/Countyapp/test_covid/"
+path <- "C:/Users/Clown Baby/Desktop/Countyapp/Countyapp/test_covid/"
 setwd(path)
 
 ################################# LOADING DATA #########################################
 
+
 ## load files, which have been cleaned and organized for this app:
-source("C:/Users/user/Desktop/Countyapp/Countyapp/test_covid/load_and_organize.R")
+source("C:/Users/Clown Baby/Desktop/Countyapp/Countyapp/test_covid/load_and_organize.R")
+
 
 ##################################### UI ###############################################
 ui <- dashboardPage(
@@ -26,14 +34,18 @@ ui <- dashboardPage(
         selectInput("selectcounty", "Select County", choices = NULL),
       #  actionButton("plot1", "plot"),
         actionButton("zoom2location","Take me there")
+    #    actionButton("go","Plot Data")
     ),
     dashboardBody( 
         tags$head(
         includeCSS("style.css"),
         includeCSS("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Source+Code+Pro:wght@200&display=swap") ),
-       # tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+        tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
         leafletOutput("map"),
-       plotOutput("plot")
+        absolutePanel (id = "controls1", class = "panel panel-default", draggable = TRUE, fixed = FALSE, top = 300, left = "auto", right = 22, bottom = "auto", height = "auto",
+                       plotOutput("plot"),
+                       textOutput("us_county_poverty")
+                      )
     )
 )
 ##################################### SERVER ###########################################
@@ -58,25 +70,47 @@ observeEvent(
    })
  
    
-#### subset counties chosen in select input (selectcounty) to display plot   
+#### subset counties chosen in select input to display plot ##  
    subset_county<- reactive({
      dplyr::filter(df, df$Admin2==input$selectcounty)
    })
-
-
+      
 ##render plot when select county 
-  
+
     output$plot <-renderPlot(
-      ggplot(data= subset_county(), 
-                        aes(x= date, y = value)) + geom_line()
+     ggplot(data= subset_county(), 
+                   aes(x= date, y = value)) + geom_line(color = "#2E9AFE", size = 1) + geom_area(fill="#3ba1ff") + ggtitle("Cases of Coronovirus") + theme(
+                     panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     panel.border = element_blank(),
+                     plot.background=element_rect(fill = '#262626'),
+                     panel.background = element_rect(fill = '#262626'),
+                     axis.text = element_text(color = 'white'),
+                     axis.line.x = element_line(color = "grey"),
+                     axis.line.y = element_line(color = "grey"),
+                     plot.title = element_text(color = 'white'),
+                     axis.title = element_text(color = 'white'),
+                     text = element_text(family = 'Courier New', size = 20)
+                   )
    )
-   
+
+####show county poverty in textOutput below plot
+##filter data based on selectcounty input 
+    subset_county_poverty <-reactive({
+      counties1 %>% filter(NAME==input$selectcounty)
+    })
+    
+##Render text based on reactive filtering function for column "All.Ages.in.Poverty.Percent"
+    output$us_county_poverty <- renderText({
+      paste0("Percentage of county in poverty (2018): ", subset_county_poverty()$All.Ages.in.Poverty.Percent, "%")
+    }) 
+    
+  
 ############ DISPLAY MAP  
    output$map <- renderLeaflet({
      
      leaflet() %>%
-       setView(lng = -88, lat = 36.1980, zoom = 8) %>% 
-       # Add tiles as baseGroup
+       setView(lng = -88, lat = 36.1980, zoom = 6) %>% 
        addProviderTiles(providers$CartoDB.DarkMatter) %>%
        addPolygons(data=counties,  
                    color = "black", 
@@ -85,12 +119,9 @@ observeEvent(
                    fillOpacity = 0.15, 
                    fillColor = "transparent",
                    group= "counties",
-                 #  popup = popupGraph("plot"),
                    layerId = ~counties1$GEOID
        ) %>% 
-       
-       #     Layers control
-       addLayersControl( baseGroups = c("Carto.DB"),
+       addLayersControl( baseGroups = c("TonerLite"),
                          overlayGroups = c(
                            "counties"
                            #   "states"),
